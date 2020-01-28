f4fd152c529354b2cb7c42063150eaa8085adca8
Felix Heinrichs <felix.heinrichs.ext@dwpbank.de>
2020-01-28 01:02:07 +0100

    added dependencies to gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,40 @@
-plugins {
-    java
-}
-
 group = "com.github.f0xdx"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+plugins {
+  java
 }
 
+repositories {
+  mavenCentral()
+}
+
+val junitVersion by extra("5.6.0")
+val kafkaVersion by extra("2.4.0")
+val lombokVersion by extra("1.18.10")
+
 dependencies {
-    compileOnly("org.projectlombok:lombok:1.18.10")
-    annotationProcessor("org.projectlombok:lombok:1.18.10")
-    testCompile("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+  // bom
+  implementation(platform("org.junit:junit-bom:${junitVersion}"))
+
+  // annotation processors
+  annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+
+  // implementation
+  compileOnly("org.projectlombok:lombok:$lombokVersion")
+  compileOnly("org.apache.kafka:connect-api:${kafkaVersion}")
+  compileOnly("org.apache.kafka:connect-transforms:${kafkaVersion}")
+
+  // test
+  testImplementation("org.apache.kafka:connect-api:${kafkaVersion}")
+  testImplementation("org.junit.jupiter:junit-jupiter-api")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
 configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
+  sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.test {
+  useJUnitPlatform()
 }
