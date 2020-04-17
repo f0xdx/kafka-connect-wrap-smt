df415e115f055046faa2b2fbc1f2a57703dd9458
Felix Heinrichs <felix.heinrichs.ext@dwpbank.de>
2020-04-17 15:25:47 +0200

    version bump junit 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 group = "com.github.f0xdx"
 version = "0.1-SNAPSHOT"
 
-val junitVersion by extra("5.6.1")
+val junitVersion by extra("5.6.2")
 val kafkaVersion by extra("2.3.1")
 val lombokVersion by extra("1.18.12")
 
